f2119e0e904e13dca38af483a3bec7dfbf467b63
shahronak1996@gmail.com <Ronak@MacBook-Air-2.local>
2016-12-21 02:07:25 -0600

    Added state and context

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -2,17 +2,50 @@ var React = require('react');
 var transparentBg = require('../styles').transparentBg;
 
 var PromptContainer = React.createClass({
+	contextTypes: { //For React-Router dynamic routing
+		router: React.PropTypes.object.isRequired
+	},
+	getInitialState() {
+		return {
+			username: ''
+		}
+	},
+	onUpdateUser(e) {
+		this.setState({
+			username: e.target.value   
+		})
+	},
+	onSubmitUser(e) {
+		e.preventDefault();
+		var username = this.state.username;
+		this.setState({
+			username: ''
+		});
+
+		if (this.props.routeParams.playerOne) {
+			this.context.router.push({
+				pathname: '/battle',
+				query: {
+					playerOne: this.props.routeParams.playerOne,
+					playerTwo: this.state.username
+				}
+			})
+		} else {
+			this.context.router.push('/playerTwo/' + this.state.username)
+		}
+	},
 	render() {
-		//console.log(this.props.route.header);
 		return (
 			<div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
 				<h1>{this.props.route.header}</h1>
 				<div className="col-sm-12">
-					<form>
+					<form onSubmit={this.onSubmitUser}>
 						<div className="form-group">
 							<input
 								className="form-control"
 								placeholder="Github Username"
+								onChange={this.onUpdateUser}
+								value={this.state.username}
 								type="text" />
 						</div>
 						<div className="form-group col-sm-4 col-sm-offset-4">
