8f7e586bf620f672082ec49ad91ded6a19bcddb3
shahronak1996@gmail.com <Ronak@MacBook-Air-2.local>
2016-12-28 21:31:24 -0600

    Added view templace for battle

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -8,7 +8,27 @@ function puke (object) {
 function ConfirmBattle (props) {
 	return props.isLoading === true
 		? <p> Loading... </p>
-		: <div> CONFIRM BATTLE!: {puke(props)}</div>  
+		: <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+			<h1>Confirm Players</h1>
+			<div className='col-sm-8 col-sm-offset-2'>
+				<div className='col-sm-6'>
+					<p className='lead'>Player 1</p>
+					PLAYER 1 STATS
+				</div>	
+				<div className='col-sm-6'>
+					<p className='lead'>Player 2</p>
+					PLAYER 2 STATS
+				</div>
+			</div>
+			<div className='col-sm-8 col-sm-offset-2'>
+				<div className='col-sm-12' style={styles.space}>
+					INITIATE BATTLE BUTTON
+				</div>
+				<div className='col-sm-12' style={styles.space}>
+					LINK TO /PLAYERNAME
+				</div>
+			</div>
+		</div>
 }; 
 
 ConfirmBattle.propTypes = {
